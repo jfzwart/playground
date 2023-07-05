88a49f51664a3245c88ba4bb8bb77c5bc75ceea9
jfzwart <jeroen.zwart@live.com>
2023-07-05 22:01:32 +0200

    add storybook and material ui

---
+++ .storybook/main.ts
@@ -0,0 +1,19 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+
+const config = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+};
+
+export default config;
